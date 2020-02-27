155a0aa31309c77e9340253d0b262b939c6d7ff1
Mir Anwar <fahimanwar@cougar.cs.ksu.edu>
2020-02-27 12:39:12 -0600

    fixed more error

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -21,6 +21,24 @@
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
+struct thread
+*get_child_process (int pid)
+{
+  struct thread *cur = thread_current();
+  struct list_elem *e;
+
+  for (e = list_begin (&cur->child_list); e != list_end(&cur->child_list); e = list_next (e))
+    {
+      struct thread *t = list_entry (e, struct thread, child_elem);
+      if (pid == t->tid)
+      {
+        return t;
+      }
+    }
+
+  return NULL;
+}
+
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
